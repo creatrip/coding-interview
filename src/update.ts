e8dcd35904aea7ca72a15af411ebb04a2942cee6
vchoisk <vchoisk@gmail.com>
2020-01-05 21:26:14 +0900

    :art: refactor to use updaterMapper

--- src/update.ts
+++ src/update.ts
@@ -1,65 +1,51 @@
-function push(state: Array<any>, value: Array<any>) {
+function $set(state: any, value: any) {
+  return value;
+}
+
+function $push(state: Array<any>, value: Array<any>) {
   return state.concat(value);
 }
+
+function $unshift(state: Array<any>, value: Array<any>) {
+  return value.concat(state);
+}
+
+function $merge(state: object, value: object) {
+  return Object.assign(state, value);
+}
+
+function $apply(state: any, value: (state: any) => any) {
+  try {
+    return value(state);
+  } catch (err) {
+    throw new Error("Error in $apply given function to state");
+  }
+}
+
+function $splice(state: any, value: Array<Array<number>>) {
+  value.forEach(spliceArgs => {
+    state.splice.apply(state, spliceArgs);
+  });
+  return state;
+}
+
+const updaterMapper: any = {
+  $set,
+  $push,
+  $unshift,
+  $merge,
+  $apply,
+  $splice
+};
 
 function update(state: any, updater: any) {
   for (let prop in updater) {
     const value = updater[prop];
     if (prop[0] === "$") {
-      switch (prop) {
-        case "$set": {
-          return value;
-        }
-        case "$push": {
-          return push(state, value);
-        }
-        case "$unshift": {
-          if (!Array.isArray(state)) {
-            throw new Error("Given state for $unshift must be array");
-          } else if (!Array.isArray(value)) {
-            throw new Error("Given updater value for $unshift must be array");
-          } else {
-            return value.concat(state);
-          }
-        }
-        case "$merge": {
-          if (typeof state !== "object") {
-            throw new Error("Given state for $merge must be object");
-          } else if (typeof value !== "object") {
-            throw new Error("Given updater value for $merge must be object");
-          } else {
-            return Object.assign(state, value);
-          }
-        }
-        case "$apply": {
-          if (typeof value !== "function") {
-            throw new Error(
-              "Given updater value for $apply must be a function"
-            );
-          } else {
-            try {
-              return value(state);
-            } catch (err) {
-              throw new Error("Error in $apply given function to state");
-            }
-          }
-        }
-        case "$splice": {
-          if (!Array.isArray(state)) {
-            throw new Error("Given state for $splice must be array");
-          } else if (!Array.isArray(value)) {
-            throw new Error("Given updater value for $splice must be array");
-          } else {
-            value.forEach(spliceArgs => {
-              state.splice.apply(state, spliceArgs);
-            });
-            return state;
-          }
-        }
-        default: {
-          console.log("given setter not defined");
-          return state;
-        }
+      try {
+        return updaterMapper[prop](state, value);
+      } catch {
+        throw new Error("Given setter is not defined");
       }
     } else {
       const newState = Object.assign({}, state);

cf5d62c8a6e829166545f42da6e54ab524f899f3
vchoisk <vchoisk@gmail.com>
2020-01-05 17:23:16 +0900

    :sparkles: implement $splice

--- src/update.js
+++ src/update.js
@@ -17,24 +17,46 @@ function update(state, updater) {
         }
         case "$unshift": {
           if (!Array.isArray(state)) {
-            throw new Error("Given state for unshift must be array");
+            throw new Error("Given state for $unshift must be array");
           } else if (!Array.isArray(value)) {
-            throw new Error("Given updater value for $push must be array");
+            throw new Error("Given updater value for $unshift must be array");
           } else {
             return value.concat(state);
           }
         }
         case "$merge": {
           if (typeof state !== "object") {
-            throw new Error("Given state for unshift must be object");
+            throw new Error("Given state for $merge must be object");
           } else if (typeof value !== "object") {
-            throw new Error("Given updater value for $push must be object");
+            throw new Error("Given updater value for $merge must be object");
           } else {
             return Object.assign({}, state, value);
           }
         }
-
-        case "apply": {
+        case "$apply": {
+          if (typeof value !== "function") {
+            throw new Error(
+              "Given updater value for $apply must be a function"
+            );
+          } else {
+            try {
+              return value(state);
+            } catch (err) {
+              throw new Error("Error in $apply given function to state");
+            }
+          }
+        }
+        case "$splice": {
+          if (!Array.isArray(state)) {
+            throw new Error("Given state for $splice must be array");
+          } else if (!Array.isArray(value)) {
+            throw new Error("Given updater value for $splice must be array");
+          } else {
+            value.forEach(spliceArgs => {
+              state.splice.apply(state, spliceArgs);
+            });
+            return state;
+          }
         }
         default: {
           console.log("given setter not defined");
